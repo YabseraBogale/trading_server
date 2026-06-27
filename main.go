50f92bbb54e4f0468896c17c698ae725eaa31031
YabseraBogale <yabserapython@gmail.com>
2026-06-27 20:20:16 +0300

    init

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type TickertHistory struct {
+	Name       string    `gorm:"name"`
+	OpenPrice  float64   `gorm:"open_price"`
+	LowPrice   float64   `gorm:"low_price"`
+	HighPrice  float64   `gorm:"high_price"`
+	ClosePrice float64   `gorm:"close_price"`
+	Volume     float64   `gorm:"volume"`
+	DatePrice  time.Time `gorm:"date_price"`
+}
+
+func (TickertHistory) TableName() string {
+	return "tickert_history"
+}
+
 func main() {
+	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	db.AutoMigrate(&TickertHistory{})
+	var result []TickertHistory
+	err = db.Select("name").Find(&result).Error
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 }
