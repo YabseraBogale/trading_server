96ca3aa447cc5091624ccd603efadecb25d70718
YabseraBogale <yabserapython@gmail.com>
2026-07-16 19:09:32 +0300

    re-write the whole project

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+import "time"
+
 type TranscationtHistory struct {
+	Name       string  `grom:"column:Name"`
+	OpenPrice  float64 `grom:"column:OpenPrice"`
+	LowPrice   float64 `grom:"column:LowPrice"`
+	HighPrice  float64 `grom:"column:HighPrice"`
+	ClosePrice float64 `grom:"column:ClosePrice"`
+	Volume     float64 `grom:"column:Volume"`
+	DatePrice  string  `grom:column:DatePrice`
 }
 
 func main() {
